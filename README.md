6572395e649b4eb196bcdef8105d7b7ff6372693
JRoTheGeek <jvrosenman@gmail.com>
2014-02-08 17:41:56 -0500

    Added some framework code for sending Ethernet frames.

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ CS 4516 Term Project
 Layer 2 Routing
 ---------------
 
+A simulation for a "smart" switch that would allow redundant links using bandwidth sharing and be auto-configuring.
+
 Dependencies:
-	* SimPy >= 3.0.2 for network simulation.
+	* impacket >= 0.9.11 for network simulation.
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # Bring everything together.
+
+
+import socket, string, sys, re, os
+from impacket import ImpactDecoder, ImpactPacket
+
+from struct import *
+
+def makepacket():
+	ethernet = ImpactPacket.Ethernet()
+	ethernet.set_ether_dhost([0xFF,0xFF,0xFF,0xFF,0xFF,0xFF])
+	ethernet.set_ether_shost([0x00,0x00,0x00,0x00,0x00,0x00])
+	ethernet.set_ether_type(0x01FF)
+	ethernet.contains("Hello, world!")
+	return ethernet.get_packet()
+
+packet = makepacket()
+s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+s.bind(("eth0", 0x01FF))
+
+while True:
+	s.send(packet)
