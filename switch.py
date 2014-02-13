551c9c12fd330de51254f3b2220a119468332c86
JRoTheGeek <jvrosenman@gmail.com>
2014-02-13 18:11:27 -0500

    Replaced tabs with spaces.~

--- switch.py
+++ switch.py
@@ -28,44 +28,44 @@ class Switch:
         # Start sniffing interface
         proc.start()
 
-	def _activate_interface(self, iface, queue):
-	    try:
-	        # Sniff specified interface for Ethernet packets and put them
-	        # into the queue
-		    sniff(filter="ether", prn=lambda(pkt):queue.put(pkt), iface=iface)
-	    except:
-	        sys.exit(1)
-	
-	def _forward_packet(self, pkt, iface):
-	    eth_header = pkt['Ethernet']
-	    # Map source port to interface
-	    # TODO: Handle multiple instances of one address
-	    hosts[eth_header.src] = iface
+    def _activate_interface(self, iface, queue):
+        try:
+            # Sniff specified interface for Ethernet packets and put them
+            # into the queue
+            sniff(filter="ether", prn=lambda(pkt):queue.put(pkt), iface=iface)
+        except:
+            sys.exit(1)
+    
+    def _forward_packet(self, pkt, iface):
+        eth_header = pkt['Ethernet']
+        # Map source port to interface
+        # TODO: Handle multiple instances of one address
+        hosts[eth_header.src] = iface
 
-	    # Check dictionary (if not a broadcast MAC) for mapping between destination and interface
-	    if(eth_header.dst !="FF:FF:FF:FF:FF:FF"):
-	        try:
-	            dst_iface = hosts[eth_header.dst]
-        	    # If mapping is found, forward frame
-	    	    return sendp(pkt, iface=dst_iface)
+        # Check dictionary (if not a broadcast MAC) for mapping between destination and interface
+        if eth_header.dst != "FF:FF:FF:FF:FF:FF":
+            try:
+                dst_iface = hosts[eth_header.dst]
+                # If mapping is found, forward frame
+                return sendp(pkt, iface=dst_iface)
             except KeyError:
                 pass
         # Otherwise, broadcast to all interfaces except the one the frame
-	    # was received on
-	    for dst_iface in queues.keys():
-	        if dst_iface != iface:
-	            sendp(pkt, iface=dst_iface)
-	    return
-		
-	def switch_forever(self):
-	    # Switch forever
-		while True:
-		    try:
-		        # For each interface, send a frame off the queue
-		        for iface, queue in queues:
-		            # Send one frame off each non-empty queue
-		            if not queue.empty()
-		                self.forward_packet(queue.get(), iface)
+        # was received on
+        for dst_iface in queues.keys():
+            if dst_iface != iface:
+                sendp(pkt, iface=dst_iface)
+        return
+        
+    def switch_forever(self):
+        # Switch forever
+        while True:
+            try:
+                # For each interface, send a frame off the queue
+                for iface, queue in queues:
+                    # Send one frame off each non-empty queue
+                    if not queue.empty():
+                        self.forward_packet(queue.get(), iface)
                 # Join any processes that may have terminated
                 for iface, process in processes:
                     # Check if any interfaces crashed
@@ -74,4 +74,4 @@ class Switch:
                     if not process.is_alive:
                         print "Interface %s terminated unexpectedly"%(iface)
             except:
-                print "Unexpected error:", sys.exc_info()[0]		
+                print "Unexpected error:", sys.exc_info()[0]        
