2451535bf050501a455e2dc8e9131f7d501ff9f1
Louis Fogel <lafogel@wpi.edu>
2014-02-13 17:50:38 -0500

    Adds actual sending of packets along with logging of interface failures

--- switch.py
+++ switch.py
@@ -1,30 +1,34 @@
 # This file will simulate a switch.
 from scapy.all import *
 from multiprocessing import Process, Queue
+import sys
 
 class Switch:
     # Dictionary mapping interface names to frame queues
     queues = {}
-    # List of active interface processes
-    processes = []
+    # Mapping of interface names to active interface processes
+    processes = {}
     # Dictionary mapping destinations to interfaces
     hosts = {}
 
     def add_interface(self, iface):
         # Initialize frame queue and map to interface
         queue = Queue()
-        queues[queue] = iface
+        queues[iface] = queue
         # Create process for sniffing interface
         proc = Process(target=self.start_switching, args=(iface,queue))
         # Add process to list
-        processes.append(proc)
+        processes[iface] = proc
         # Start sniffing interface
         proc.start()
 
 	def activate_interface(self, iface, queue):
-	    # Sniff specified interface for Ethernet packets and put them
-	    # into the queue
-		sniff(filter="ether", prn=lambda(pkt):queue.put(pkt), iface=iface)
+	    try:
+	        # Sniff specified interface for Ethernet packets and put them
+	        # into the queue
+		    sniff(filter="ether", prn=lambda(pkt):queue.put(pkt), iface=iface)
+	    except:
+	        sys.exit(1)
 	
 	def forward_packet(self, pkt, iface):
 	    eth_header = pkt['Ethernet']
@@ -36,26 +40,31 @@ class Switch:
 	    if(eth_header.dst !="FF:FF:FF:FF:FF:FF"):
 	        try:
 	            dst_iface = hosts[eth_header.dst]
-        	    # TODO: If mapping is found, forward frame
-	    	    raise RuntimeError("Unimplimented")
-	    	    return
+        	    # If mapping is found, forward frame
+	    	    return sendp(pkt, iface=dst_iface)
             except KeyError:
                 pass
-        # TODO: Otherwise, broadcast to all interfaces except the one the frame
+        # Otherwise, broadcast to all interfaces except the one the frame
 	    # was received on
-	    raise RuntimeError("Unimplimented")
+	    for dst_iface in queues.keys():
+	        if dst_iface != iface:
+	            sendp(pkt, iface=dst_iface)
 	    return
 		
 	def switch_forever(self):
 	    # Switch forever
 		while True:
 		    # For each interface, send a frame off the queue
-		    for queue, iface in queues:
+		    for iface, queue in queues:
 		        # Send one frame off each non-empty queue
 		        if not queue.empty()
 		            self.forward_packet(queue.get(), iface)
             # Join any processes that may have terminated
-            for process in processes:
+            for iface, process in processes:
+                # Check if any interfaces crashed
                 process.join(None)
+                # If the interface process is dead, log it
+                if not process.is_alive:
+                    print "Interface %s terminated unexpectedly"%(iface)
 		            
 		
