4ebc6d1a9a100a8180168f03dae76d6302d5ad8b
JRoTheGeek <jvrosenman@gmail.com>
2014-02-13 15:57:30 -0500

    Started work on switch framework.

--- switch.py
+++ switch.py
@@ -1,1 +1,10 @@
 # This file will simulate a switch.
+
+from scapy.all import *
+
+class Switch:
+	def start_switching(self):
+		sniff(filter="ether", prn=self.forward_packet)
+		
+	def forward_packet(self):
+		pass
