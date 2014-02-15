c5f21d454fd902a605432b9b388489841ff4ca93
sam abradi <stabradi@wpi.edu>
2014-02-15 12:26:20 -0500

    added a thing that should hash the frames and check if they are in the list of the last N things that went out that interface as broadcast and not send them out if they have already gone that way recently. I have not yet gotten around to testing it. Does it work? probably. Is it correct Python? I hope so. Am I going to test it? Probably at some point. Possibly while drunk, the testbed likes me when I am drunk

--- fancy_switch.py
+++ fancy_switch.py
@@ -3,11 +3,30 @@ from scapy.all import *
 from multiprocessing import Process, Queue
 import sys, traceback
 import logging
+import collections
 from switch import Switch
+
+# this is the number of checksums for an interface to store
+checker_backlog = 16 # networking people and the number 16, LOL
 
 logging.getLogger("scapy").setLevel(logging.ERROR)
 
 class FancySwitch(Switch):
+    # dict of circular buffers of frames that have been sent out interface x, indexed by interface
+    sent_frames = {}
+    def _add_interface(self, iface):
+        # Initialize frame queue and map to interface
+        queue = Queue()
+        self.queues[iface] = queue
+        # make the circular buffer to check against before broadcasting something
+        self.sent_frames[iface] = collections.deque(maxlen=checker_backlog)
+        # Create process for sniffing interface
+        proc = Process(target=self._activate_interface, args=(iface,queue))
+        # Add process to list
+        self.processes[iface] = proc
+        # Start sniffing interface
+        proc.start()
+
     def _forward_packet(self, pkt, iface):
         eth_header = pkt['Ethernet']
         # Map source port to interface
@@ -32,6 +51,9 @@ class FancySwitch(Switch):
         # was received on
         for dst_iface in self.queues.keys():
             if dst_iface != iface:
-                #print "%s -> %s (bcast) on %s -> %s" %(eth_header.src, eth_header.dst, iface, dst_iface)
-                sendp(pkt, iface=dst_iface, verbose=False)
+                pkt_hash = pkt.hashret()
+                if pkt_hash not in self.sent_frames[dst_iface]:
+                    self.sent_frames[dst_iface].append(pkt_hash)
+                    #print "%s -> %s (bcast) on %s -> %s" %(eth_header.src, eth_header.dst, iface, dst_iface)
+                    sendp(pkt, iface=dst_iface, verbose=False)
         return
