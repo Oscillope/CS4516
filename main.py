3f74bc3486a07aa47867b3e3a0de920ffe269a81
Sam Abradi <stabradi@wpi.edu>
2014-03-02 22:09:03 -0500

    added a better command line parser for main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import sys
 dev_string = ["eth1", "eth2", "eth3"]
 #dev_string = ["eth0", "eth1", "eth2", "eth3"]
 
-if len(sys.argv) == 1:
-    s = Switch(dev_string)
+if len(sys.argv) < 4:
+    print('usage: \nstd: launch a stadard switch\nadv: launch one of our fancy switches')
 else:
+    dev_string = sys.argv[2:]
     if sys.argv[1] == 'std':
         s = Switch(dev_string)
     elif sys.argv[1] == 'adv':
