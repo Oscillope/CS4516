849315fbdd41f1f19221e626027d1a841a811a3f
Sam Abradi <stabradi@wpi.edu>
2014-03-02 22:21:00 -0500

    hey, have a better ussage message

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dev_string = ["eth1", "eth2", "eth3"]
 #dev_string = ["eth0", "eth1", "eth2", "eth3"]
 
 if len(sys.argv) < 4:
-    print('usage: \nstd: launch a stadard switch\nadv: launch one of our fancy switches')
+    print('usage: [switch type] [interface 1] [interface 2] ...\ndevice types:\n  std: launch a stadard switch\n  adv: launch one of our fancy switches')
 else:
     dev_string = sys.argv[2:]
     if sys.argv[1] == 'std':
